e17c1c45304f9892b7b74172a23dbc86152cd45a
priyanshu5402 <priyanshu.alld2014@gmail.com>
2025-07-19 00:29:18 +0530

    auth-service added

--- analytics-service/src/main/java/com/pm/analytics_service/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analytics_service/kafka/KafkaConsumer.java
@@ -1,4 +1,25 @@
 package com.pm.analytics_service.kafka;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
 public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event){
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+
+            log.info("Listening patient event: [patientName: {}, patientEmail: {}]", patientEvent.getName(), patientEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error parsing the byte code {}", e.getMessage());
+        }
+    }
 }
